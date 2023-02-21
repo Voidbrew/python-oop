d4c10ae0c39fe1b0cf46bde4c0d5e0d0769014e0
Rabbit in a hat <42340965+Voidbrew@users.noreply.github.com>
2023-02-21 13:38:26 +0100

    Add files via upload

---
+++ singleton.py
@@ -0,0 +1,21 @@
+import time
+
+class Singleton:
+    def __new__(cls):
+        if hasattr(cls,'instance'):
+            return cls.instance
+            
+        cls.instance = super(Singleton,cls).__new__(cls)
+        cls.printable = 0
+
+    def print(self):
+        return self.printable
+
+start_time = time.time()
+
+Singleton()
+for i in range(10000000):
+    Singleton.a = i
+Singleton().print()
+
+print(time.time()-start_time)
